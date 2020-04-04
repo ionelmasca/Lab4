0d1816fa338e33dd9fff4df79eb8a2891b8c85af
nicoararaul <62560267+nicoararaul@users.noreply.github.com>
2020-04-04 23:01:49 +0300

    Update UI.cpp
    functia UserInterface

--- UI.cpp
+++ UI.cpp
@@ -66,3 +66,65 @@ void UI::sortare_afisare()
 	undo.push_back(3);
 }
 
+void UI::UserInterface()
+{
+	string x;
+	std::cout << "Pentru a adauga un medicament apasati 1" << endl;
+	std::cout << "Pentru a sterge un element apasati 2" << endl;
+	std::cout << "Pentru a viziona Medicamentele apasati tasta 3" << endl;
+	std::cout << "Pentru a afisa elementele cu un anumit string apasati tasta 4" << endl;
+	std::cout << "Pentru a afisa medicamente cu o anumita cantitate apasati tasta 5" << endl;
+	std::cout << "Pentru a sorta dupa pret apasati tasta 6" << endl;
+	std::cout << "Pentru UNDO apasati tasta 7" << endl;
+	std::cin >> x;
+
+	while (x != "8")
+	{
+		if (x == "1")
+		{
+			add();
+		}
+		if (x == "2")
+		{
+			deleteMedicament();
+		}
+		if (x == "3")
+		{
+			farmacie.display_medicamente();
+		}
+		if (x == "4")
+		{
+			display_anumite_medicamente();
+		}
+		if (x == "5")
+		{
+			farmacie.under_menge();
+		}
+		if (x == "6")
+		{
+			sortare_afisare();
+		}
+		if (x == "7")
+		{
+			if (undo[undo.size() - 1] == 1)
+			{
+				farmacie.delete_last_medicament();
+			}
+			if (undo[undo.size() - 1] == 2)
+			{
+
+				farmacie.add_medicament(lastmedicament[lastmedicament.size() - 1]);
+				lastmedicament.pop_back();
+			}
+			undo.pop_back();
+		}
+		cout << "Pentru a adauga un medicament apasati 1" << endl;
+		cout << "Pentru a sterge un element apasati 2" << endl;
+		cout << "Pentru a viziona Medicamentele apasati tasta 3" << endl;
+		cout << "Pentru a afisa elementele cu un anumit string apasati tasta 4" << endl;
+		cout << "Pentru a afisa medicamente cu o anumita cantitate apasati tasta 5" << endl;
+		cout << "Pentru a sorta dupa pret apasati tasta 6" << endl;
+		cout << "Pentru UNDO apasati tasta 7" << endl;
+		cin >> x;
+	}
+}
