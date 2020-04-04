c63b2315b4f6a555d88b57d4cddc9296e7fea51f
ionelmasca <ionel_masca@yahoo.com>
2020-04-04 22:57:56 +0300

    Update App.cpp
    Update APP

--- App.cpp
+++ App.cpp
@@ -1,36 +1,14 @@
 #include <iostream>
-#include "ShortTest.h"
-#include "SortedIteratedList.h"
-#include "ListIterator.h"
-#include "ExtendedTest.h"
-using namespace std;
+#include "Medicament.h"
+#include "Apotheke.h"
+#include "Tests.h"
+#include "UI.h"
 
-Relation agb;
-
-
-int main() 
+int main()
 {
-	
-	SortedIteratedList l(agb);
 
-	l.add(9);
-	l.add(20);
-	l.add(15);
-	l.add(17);
-	l.add(18);
-	l.add(90);
-	l.add(7);
-	
-	//ListIterator p = l.search(9);
-
-
-	//l.remove(p);
-	//l.afisare();
-
-	//testAll();
-	//cout << "abc"<<endl; 
-	testAllExtended();
-	std::cout << "Finished ALL Tests!" << std::endl;
-	system("pause");
+	//testall();
+	UI t;
+	t.UserInterface();
 	return 0;
 }
